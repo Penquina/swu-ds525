e7d95e32d3b16447154b81c2e6d6ca40ca15870a
Penquina <peeyapak.somvitoon@g.swu.ac.th>
2022-09-01 18:12:14 +0000

    Update table

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -6,6 +6,7 @@ from typing import List
 from cassandra.cluster import Cluster
 
 table_drop = "DROP TABLE events"
+#1. เพิ่ม field : created_at text : เพราะจะเก็บข้อมูลfiled เพิ่ม
 
 table_create = """
     CREATE TABLE IF NOT EXISTS events
@@ -13,6 +14,7 @@ table_create = """
         id text,
         type text,
         public boolean,
+        created_at text, 
         PRIMARY KEY (
             id,
             type
@@ -42,7 +44,6 @@ def create_tables(session):
         except Exception as e:
             print(e)
 
-
 def get_files(filepath: str) -> List[str]:
     """
     Description: This function is responsible for listing the files in a directory
@@ -59,35 +60,46 @@ def get_files(filepath: str) -> List[str]:
 
     return all_files
 
-
+#5. Read JSON แล้วprintout ข้อมูลที่เซตที่ 3มาดู
 def process(session, filepath):
     # Get list of files from filepath
     all_files = get_files(filepath)
 
     for datafile in all_files:
         with open(datafile, "r") as f:
+            #load file มาอ่าน
             data = json.loads(f.read())
             for each in data:
                 # Print some sample data
-                print(each["id"], each["type"], each["actor"]["login"])
+                #5.จากเดิม print(each["id"], each["type"], each["actor"]["login"])
+                #  ล้อตาม2.ที่ insert_sample_data เปลี่ยนเป็น
+                # Print มาดู ยังไม่ได้ใส่ตาราง
+                print(each["id"], each["type"], each["public"], each["created_at"])
 
                 # Insert data into tables here
+                #6. ใส่ข้อมูลในตาราง
+                query = f"""
+                INSERT INTO events (id, type, public, created_at) VALUES ('{each["id"]}', '{each["type"]}', {each["public"]}, '{each["created_at"]}' )
+                """
+                session.execute(query)
 
+
+# 2. เพิ่ม ค่าใน created_at เข้ามา "2022-08-17T15:51:05Z"
 
 def insert_sample_data(session):
     query = f"""
-    INSERT INTO events (id, type, public) VALUES ('23487929637', 'IssueCommentEvent', true)
+    INSERT INTO events (id, type, public, created_at) VALUES ('23487929661', 'PushEvent', true, '2022-08-17T15:51:05Z' )
     """
     session.execute(query)
 
 
 def main():
-    cluster = Cluster(['127.0.0.1'])
-    session = cluster.connect()
+    cluster = Cluster(['127.0.0.1'])    # Connect casada 
+    session = cluster.connect()         # Create session
 
     # Create keyspace
     try:
-        session.execute(
+        session.execute(               #Create dB
             """
             CREATE KEYSPACE IF NOT EXISTS github_events
             WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
@@ -98,20 +110,28 @@ def main():
 
     # Set keyspace
     try:
-        session.set_keyspace("github_events")
+        session.set_keyspace("github_events")    
     except Exception as e:
         print(e)
 
-    drop_tables(session)
-    create_tables(session)
+    drop_tables(session)    #drop table
+    #Prepare for create new table ถ้าอยากเก็บข้อมูล field เพิ่มต้องสร้างfield ใหม่ ไปที่ table_create
+    create_tables(session)  
 
-    # process(session, filepath="../data")
+    #4.Test แล้วใช้ได้ จะนำเข้าจริง เซตpath ไปที่ directory data ที่เก็บ json
+    process(session, filepath="../data")
+    # ใส่ข้อมูล hard code เข้าไปเพื่อทดสอบว่าสามารถใส่ข้อมูลในตารรางได้มั้ย เป็นขั้น 2 ขฬ เรียก function : insert_sample_data
+
     insert_sample_data(session)
 
     # Select data in Cassandra and print them to stdout
+    #3. Select event เปลี่ยน id เป็น "id": "23487929661" , เปลี่ยน type เป็น "type": "PushEvent"
+    # เดิมเป็น SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
     query = """
-    SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
+    SELECT * from events WHERE id = '23487929661' AND type = 'PushEvent'
     """
+
+    
     try:
         rows = session.execute(query)
     except Exception as e:
@@ -120,6 +140,6 @@ def main():
     for row in rows:
         print(row)
 
-
+# Main 
 if __name__ == "__main__":
     main()
